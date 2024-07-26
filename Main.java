ab32f51b92950d13fff0adac16f87a87a3ab478b
MarianaGaldino <marianagaldino1@gmail.com>
2024-07-26 20:20:49 -0300

    adiciona metodo para imprimir mensagem adicional

--- Main.java
+++ Main.java
@@ -2,5 +2,9 @@ public class Main{
 	public static void main(String[] args){
 		System.out.println("Hello World!");
 		System.out.println("Bem vindo ao Git!");
+		printAdditionalMessage();
+	}
+	public static void printAdditionalMessage(){
+		System.out.println("Esta é uma mensagem adicional");
 	}
 }
